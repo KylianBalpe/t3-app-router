2b564ac5d752209a9f432278437d6f6140edb36e
KylianBalpe <balpamula@gmail.com>
2024-09-29 22:21:46 +0700

    feat: change to github login

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,5 @@
 import { postRouter } from "@/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
-import { authRouter } from "@/server/api/routers/auth";
 
 /**
  * This is the primary router for your server.
@@ -9,7 +8,6 @@ import { authRouter } from "@/server/api/routers/auth";
  */
 export const appRouter = createTRPCRouter({
   post: postRouter,
-  auth: authRouter,
 });
 
 // export type definition of API
